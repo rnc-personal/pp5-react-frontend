d6be53eb44728dd86a3c595b2c4cbc98d7c1194e
rnc-personal <richard.challenger@protonmail.com>
2023-07-20 20:34:27 +0000

    ADD Sign In Form and basic styling

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,14 +1,12 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignUp.module.css";
 import btnStyles from "../../styles/Button.module.css";
@@ -16,46 +14,78 @@ import appStyles from "../../App.module.css";
 
 function SignInForm() {
 
-    return (
-        <>
-            <Row className={styles.Row}>
-                <Col className="my-auto p-0 p-md-2" md={6}>
-                    <Container className={`${appStyles.Content} p-4 `}>
-                        <h1 className={styles.Header}>SIGN IN</h1>
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: "",
+    });
 
-                        <Form>
-                            <Form.Group controlId="username">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Username" name="username" className={styles.Input}/>
-                            </Form.Group>
+    const { username, password } = signInData;
+    const [errors, setErrors] = useState({});
+    const history = useHistory();
 
-                            <Form.Group controlId="password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="password" className={styles.Input}/>
-                            </Form.Group>
-                            <Button type="submit">
-                                SUBMIT
-                            </Button>
-                        </Form>
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-                    </Container>
-                    <Container className={`mt-3 ${appStyles.Content}`}>
-                        <Link className={styles.Link} to="/signup">
-                            Don't have an account? <span>Sign up now!</span>
-                        </Link>
-                    </Container>
-                </Col>
-            </Row>
-            <Row className={styles.Row}>
-                <Col
-                    md={6}
-                    className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-                >
-                    <h2>PlaceHolder for IMG/Cube</h2>
-                </Col>
-            </Row>
-        </>
-    );
-}
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/login/", signInData);
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data)
+        };
+    }
 
-export default SignInForm;
+        return (
+            <>
+                <Row className={styles.Row}>
+                    <Col className="my-auto p-0 p-md-2" md={6}>
+                        <Container className={`${appStyles.Content} p-4 `}>
+                            <h1 className={styles.Header}>SIGN IN</h1>
+
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId="username">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter Username" name="username" className={styles.Input} value={username} onChange={handleChange} />
+                                </Form.Group>
+
+                                {errors.username?.map((message, idx) => (<p key={idx} >{message}</p>))}
+
+                                <Form.Group controlId="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" name="password" className={styles.Input} value={password} onChange={handleChange} />
+                                </Form.Group>
+
+                                {errors.password?.map((message, idx) => (<p key={idx} >{message}</p>))}
+
+                                <Button type="submit"  className={btnStyles.Button}>
+                                    SUBMIT
+                                </Button>
+                                {errors.non_field_errors?.map((message, idx) => (<p key={idx} className="mt-3">{message}</p>))}
+                            </Form>
+
+                        </Container>
+                        <Container className={`mt-3 ${appStyles.Content}`}>
+                            <Link className={styles.Link} to="/signup">
+                                Don't have an account? <span>Sign up now!</span>
+                            </Link>
+                        </Container>
+                    </Col>
+                </Row>
+                <Row className={styles.Row}>
+                    <Col
+                        md={6}
+                        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+                    >
+                        <h2>PlaceHolder for IMG/Cube</h2>
+                    </Col>
+                </Row>
+            </>
+        );
+    }
+
+    export default SignInForm;
