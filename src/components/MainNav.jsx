fdb1498769807288cd3dfc6b7f70eef503a00bba
rnc-personal <richard.challenger@protonmail.com>
2023-07-20 18:04:57 +0000

    FIX: Styles broken on Nav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,43 +2,43 @@ import React from "react"
 import { Container, Navbar, Nav } from "react-bootstrap"
 import { NavLink } from "react-router-dom";
 import RGBBar from '../components/RGBBar';
-import styles from "../styles/MainNav.module.css"
+import stylesNav from "../styles/MainNav.module.css"
 
 const MainNav = () => {
     return (
-        <Navbar expand="md" fixed="top" className={styles.MainNav} >
+        <Navbar expand="md" fixed="top" className={stylesNav.MainNav} >
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
                         <h1>BATTLEBOXES</h1>
                     </Navbar.Brand>
                 </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.NavBarToggler} />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className={stylesNav.NavBarToggler} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink
                             exact
-                            className={styles.NavLink}
-                            activeClassName={styles.NavLinkActive}
+                            className={stylesNav.NavLink}
+                            activeClassName={stylesNav.NavLinkActive}
                             to="/">
                             Home
                         </NavLink>
                         <NavLink
-                            className={styles.NavLink}
-                            activeClassName={styles.NavLinkActive}
+                            className={stylesNav.NavLink}
+                            activeClassName={stylesNav.NavLinkActive}
                             to="/signin">
                             Sign in
                             </NavLink>
                         <NavLink
-                            className={styles.NavLink}
-                            activeClassName={styles.NavLinkActive}
+                            className={stylesNav.NavLink}
+                            activeClassName={stylesNav.NavLinkActive}
                             to="/signup">
                             Sign up
                             </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-            <RGBBar className={styles.RGBBar} />
+            <RGBBar className={stylesNav.RGBBar} />
         </Navbar>
     )
 }
