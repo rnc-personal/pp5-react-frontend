f62732fcaf79f473e7a73118e7c69cf008c37ca4
rnc-personal <richard.challenger@protonmail.com>
2023-07-20 17:31:56 +0000

    ADD: Fields for Signup Form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -8,31 +8,50 @@ import appStyles from "../../App.module.css";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
-  return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-auto py-2 p-md-2" md={6}>
+                <Container className={`${appStyles.Content} p-4 `}>
+                    <h1 className={styles.Header}>SIGN UP</h1>
 
-            {/* add your form here */}
+                    <Form>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">USERNAME:</Form.Label>
+                            <Form.Control type="TEXT" placeholder="USERNAME" name="username"/>
+                        </Form.Group>
 
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <div>
-            IMG/Cube Here
-            </div>
-      </Col>
-    </Row>
-  );
+                        <Form.Group controlId="password-1">
+                            <Form.Label className="d-none">PASSWORD</Form.Label>
+                            <Form.Control type="password-1" placeholder="SET A PASSWORD" name="password-1"/>
+                        </Form.Group>
+
+                        <Form.Group controlId="password-2">
+                            <Form.Label className="d-none">CONFIRM PASSWORD</Form.Label>
+                            <Form.Control type="password" placeholder="CONFIRM YOUR PASSWORD" name="password-2"/>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+
+                </Container>
+                <Container className={`mt-3 ${appStyles.Content}`}>
+                    <Link className={styles.Link} to="/signin">
+                        Already have an account? <span>Sign in</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col
+                md={6}
+                className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+            >
+                <div>
+                    IMG/Cube Here
+                </div>
+            </Col>
+        </Row>
+    );
 };
 
 export default SignUpForm;
