0a3cf7684818cb58be20c0ec3c37ff0fcf84132d
rnc-personal <richard.challenger@protonmail.com>
2023-07-20 22:12:04 +0000

    UPD: Added AXios Interceptors/Tokn Refresh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://pp5-django-api-b2580fe4fff7.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
