f5d6044a7595d9c48641242ed9506a0849dd722e
rnc-personal <richard.challenger@protonmail.com>
2023-07-20 19:15:06 +0000

    FIX: Added Extra error reporting for Sign-In Attempt (was CORS issue in API)

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignUp.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
-
+import axios from "axios";
 
 const SignUpForm = () => {
 
@@ -15,12 +15,31 @@ const SignUpForm = () => {
     })
 
     const { username, password1, password2 } = signUpFormData;
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
 
     const handleChange = (event) => {
         setSignUpFormData({
             ...signUpFormData,
             [event.target.name]: event.target.value,
         });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/registration/", signUpFormData);
+            history.push("/");
+        } catch (err) {
+            const responseErrors = err.response?.data;
+            if (responseErrors) {
+              setErrors(responseErrors);
+              console.log(`Error on Submit: ${JSON.stringify(responseErrors)}`);
+            } else {
+              console.log("Unknown error occurred:", err);
+            }
+          }
     };
 
     return (
@@ -30,21 +49,27 @@ const SignUpForm = () => {
                     <Container className={`${appStyles.Content} p-4 `}>
                         <h1 className={styles.Header}>SIGN UP</h1>
 
-                        <Form>
+                        <Form onSubmit={handleSubmit} >
                             <Form.Group controlId="username">
                                 <Form.Label className="d-none">USERNAME:</Form.Label>
                                 <Form.Control className={styles.Input} type="TEXT" placeholder="USERNAME" name="username" value={username} onChange={handleChange} />
                             </Form.Group>
+
+                            {errors.username?.map((message, idx) => (<p key={idx}>{message}</p>))}
 
                             <Form.Group controlId="password-1">
                                 <Form.Label className="d-none">PASSWORD</Form.Label>
                                 <Form.Control className={styles.Input} type="password-1" placeholder="SET A PASSWORD" name="password1" value={password1} onChange={handleChange} />
                             </Form.Group>
 
+                            {errors.password1?.map((message, idx) => (<p key={idx}>{message}</p>))}
+
                             <Form.Group controlId="password-2">
                                 <Form.Label className="d-none">CONFIRM PASSWORD</Form.Label>
                                 <Form.Control className={styles.Input} type="password" placeholder="CONFIRM YOUR PASSWORD" name="password2" value={password2} onChange={handleChange} />
                             </Form.Group>
+
+                            {errors.password2?.map((message, idx) => (<p key={idx}>{message}</p>))}
 
                             <Button className={btnStyles.Button} type="submit">
                                 SUBMIT
