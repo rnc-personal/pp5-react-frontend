e9c51150e2c8ae7097a8f9e1a14ebc020193a0fe
rnc-personal <richard.challenger@protonmail.com>
2023-07-21 17:28:04 +0000

    ADD: Create Build Form Boilerplate / CSS FIles and placeholder asset

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainNav from './components/MainNav';
 import Container from 'react-bootstrap/Container';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import CreateBuildForm from './pages/builds/CreateBuildForm';
 import styles from './App.module.css';
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
@@ -20,6 +21,7 @@ function App() {
           } />
           <Route exact path='/signin' render={() => <SignInForm />} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <CreateBuildForm />} />
           <Route render={() =>
             <h1>Whoops Looks You're Lost!</h1>
           } />
