fa1033fc366762aaa6a11f2e40d46aa884de36aa
rnc-personal <richard.challenger@protonmail.com>
2023-07-22 10:44:48 +0000

    ADD: Build Image Upload

--- src/pages/builds/CreateBuildForm.js
+++ src/pages/builds/CreateBuildForm.js
@@ -16,6 +16,37 @@ import inputStyles from "../../styles/SignUp.module.css";
 
 function PostCreateForm() {
 
+  const [postData, setPostData] = useState({
+    build_name: "",
+    content: "",
+    main_image: "",
+    build_cpu: "",
+    build_mobo: "",
+    build_ram: "",
+    build_disk: "",
+    build_gpu: "",
+    build_monitor: "",
+  });
+
+  const { build_name, content, main_image, build_cpu, build_mobo, build_ram, build_disk, build_gpu, build_case, build_monitor } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
   const [errors, setErrors] = useState({});
 
   const textFields = (
@@ -83,48 +114,50 @@ function PostCreateForm() {
 
             <Form.Group className="text-center">
               <Form.Label>BUILD NAME</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_name" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_name" value={build_name}/>
             </Form.Group>
 
             <Form.Group className="text-center">
-            <Form.Label>TELL US ABOUT THE BUILD - PROCESS, ISSUES ETC...</Form.Label>
-              <Form.Control className={inputStyles.Input} as="textarea" rows={8} name="content" />
+              <Form.Label>TELL US ABOUT THE BUILD - PROCESS, ISSUES ETC...</Form.Label>
+              <Form.Control onChange={handleChange} className={inputStyles.Input} as="textarea" rows={8} name="content" value={content}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>CPU</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_cpu" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_cpu" value={build_cpu}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>MOTHERBOARD</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_mobo" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_mobo" value={build_mobo}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>RAM</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_ram" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_ram" value={build_ram}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>STORAGE</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_disk" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_disk" value={build_disk}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>GPU</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_gpu" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_gpu" value={build_gpu}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>CASE</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_case" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_case" value={build_case}/>
             </Form.Group>
 
             <Form.Group className="text-center">
               <Form.Label>MONITOR</Form.Label>
-              <Form.Control className={inputStyles.Input} type="text" name="build_monitor" />
+              <Form.Control onChange={handleChange} className={inputStyles.Input} type="text" name="build_monitor" value={build_monitor} />
             </Form.Group>
+
+            <Form.File onChange={handleChangeImage} id="image-upload"accept="image/*"/>
 
             <div className="d-md-none">{textFields}</div>
           </Container>
