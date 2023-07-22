f4ac8efc8fffc5c1a882e937ac0a755638bc3ee9
rnc-personal <richard.challenger@protonmail.com>
2023-07-22 18:27:19 +0000

    FIX: Issue where Promise Data was not returned

--- src/pages/builds/BuildDetailPage.js
+++ src/pages/builds/BuildDetailPage.js
@@ -11,17 +11,18 @@ import appStyles from "../../App.module.css";
 
 function PostPage() {
 const { id } = useParams();
-const [build, setBuild] = useState({ results: [] });
+const [ build , setBuild ] = useState({ results: [] });
 
 useEffect(() => {
     const handleMount = async () => {
       try {
         const [{ data: build }] = await Promise.all([
-          axiosReq.get(`/builds/${id}`),
-        //   axiosReq.get(`/comments/?post=${id}`),
-        ]);
+        axiosReq.get(`/builds/${id}`),
+    ]);
+
         setBuild({ results: [build] });
-        // setComments(comments);
+        console.log(build)
+        
       } catch (err) {
         console.log(err);
       }
