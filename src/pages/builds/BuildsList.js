a35f8605c38168f272faa19f8857d7db837ff2b7
rnc-personal <richard.challenger@protonmail.com>
2023-07-24 19:58:38 +0000

    ADD: No Results Asset

--- src/pages/builds/BuildsList.js
+++ src/pages/builds/BuildsList.js
@@ -6,9 +6,10 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Build from "./Build";
 
 import appStyles from "../../App.module.css";
-import Build from "./Build";
+import noResults from "../../assets/not_found_sm.png";
 
 function BuildsList({ message, filter = "" }) {
 
@@ -18,43 +19,47 @@ function BuildsList({ message, filter = "" }) {
 
     useEffect(() => {
         const fetchBuilds = async () => {
-          try {
-            // TO DO: add filter
-            const { data } = await axiosReq.get(`/builds/`);
-            setBuilds(data);
-            setHasLoaded(true);
-          } catch (err) {
-            console.log(err);
-          }
+            try {
+                // TO DO: add filter
+                const { data } = await axiosReq.get(`/builds/`);
+                setBuilds(data);
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
         };
-    
+
         setHasLoaded(false);
         const timer = setTimeout(() => {
-          fetchBuilds();
+            fetchBuilds();
         }, 1000);
-    
+
         return () => {
-          clearTimeout(timer);
+            clearTimeout(timer);
         };
-      }, []);
+    }, []);
 
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles mobile</p>
-            {hasLoaded ? (
-             <>
-             {builds.results.length ? (
-                builds.results.map(build => (
-                    <Build key={build.id} {...build} setBuilds={setBuilds}/>
-                ))
-             ) : (
-                console.log('not loaded')
-             )}
-             </>   
-            ): (
-                <p>Loading...</p>
-            )}
+                {hasLoaded ? (
+                    <>
+                        {builds.results.length ? (
+                            builds.results.map(build => (
+                                <Build key={build.id} {...build} setBuilds={setBuilds} />
+                            ))
+                        ) : (
+                            <img src={noResults} alt="no results" message={message} />
+                        )
+                        }
+                    </>
+                ) : (
+                    <>
+                        <p>Loading...</p>
+                        <img src={noResults} alt="loading" message={message} />
+                    </>
+                )}
 
             </Col>
             <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
