e83502c2845d87eb66a75019f3353ed4823e8664
rnc-personal <richard.challenger@protonmail.com>
2023-07-24 19:23:25 +0000

    ADD: Setting Up State for List page

--- src/pages/builds/BuildsList.js
+++ src/pages/builds/BuildsList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,19 +8,24 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 
-function BuildsList() {
-  
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of builds here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
-  );
+function BuildsList({ message, filter = "" }) {
+
+    const [builds, setBuilds] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Popular profiles mobile</p>
+                <p>List of builds here</p>
+            </Col>
+            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+                <p>Popular profiles for desktop</p>
+            </Col>
+        </Row>
+    );
 }
 
 export default BuildsList;
