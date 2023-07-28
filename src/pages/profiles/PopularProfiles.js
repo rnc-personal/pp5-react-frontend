1e604c9ff8046ff2fb7fbe2c6079c328ac0d0ed2
rnc-personal <richard.challenger@protonmail.com>
2023-07-28 13:22:42 +0000

    UPD: Refactor for Profile Context Hook

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -3,34 +3,13 @@ import appStyles from '../../App.module.css'
 import { Container } from 'react-bootstrap'
 import { axiosReq } from '../../api/axiosDefaults'
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Asset from '../../components/Asset'
 import Profile from "./Profile";
 
 const PopularProfiles = ({mobile}) => {
 
-    const [profileData, setProfileData] = useState({
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    })
-
-    const { popularProfiles } = profileData
-    const currentUser = useCurrentUser
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const {data} = await axiosReq.get('profiles/?ordering=-followers_count')
-                setProfileData(prevState => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }))
-            }
-            catch (error) {
-                console.log(error)
-            }
-        };
-        handleMount()
-    }, [currentUser])
+    const { popularProfiles } = useProfileData();
 
     return (
         <Container
