716a756674a2f2095836bbe65dc8e7407e9b429f
rnc-personal <richard.challenger@protonmail.com>
2023-07-29 13:21:33 +0000

    UPD: Formatting in Utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,7 +17,7 @@ export const fetchMoreData = async (resource, setResource) => {
 
   export const followHelper = (profile, clickedProfile, following_id) => {
     return profile.id === clickedProfile.id
-      ? {...profile, followers_count: profile.followers_count + 1,following_id,}
+      ? {...profile, followers_count: profile.followers_count + 1, following_id,}
       : profile.is_owner
       ? { ...profile, following_count: profile.following_count + 1 }
       : profile;
