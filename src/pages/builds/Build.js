100dabbad7abe8971880bf9af60a0398b452028a
rnc-personal <richard.challenger@protonmail.com>
2023-09-24 12:45:50 +0000

    RESUB: Commented Out Debug Images

--- src/pages/builds/Build.js
+++ src/pages/builds/Build.js
@@ -10,14 +10,15 @@ import styles from '../../styles/BuildDetail.module.css';
 import { Player } from '@lottiefiles/react-lottie-player';
 import BuildGallery from '../../components/BuildGallery';
 
-import img1 from '../../assets/1.png'
-import img2 from '../../assets/2.png'
-import img3 from '../../assets/3.png'
-import img4 from '../../assets/4.png'
-import img5 from '../../assets/5.png'
-import img6 from '../../assets/6.png'
-import img7 from '../../assets/7.png'
-import img8 from '../../assets/8.png'
+// Debug Images
+// import img1 from '../../assets/1.png'
+// import img2 from '../../assets/2.png'
+// import img3 from '../../assets/3.png'
+// import img4 from '../../assets/4.png'
+// import img5 from '../../assets/5.png'
+// import img6 from '../../assets/6.png'
+// import img7 from '../../assets/7.png'
+// import img8 from '../../assets/8.png'
 
 
 const Build = (props) => {
