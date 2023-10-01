98a7eee08f1fa3351ea90145d23033597d8a3339
rnc-personal <richard.challenger@protonmail.com>
2023-10-01 13:16:07 +0000

    RESUB: Removing Unused Imports (Rating)

--- src/pages/ratings/Rating.js
+++ src/pages/ratings/Rating.js
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "../../styles/Comment.module.css";
-import { Popout } from "../../components/Popout";
-
 
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
-import CommentEditForm from "../comments/CommentEditForm";
+
 
 const Rating = (props) => {
     const { id, creator, rating_value } = props;
